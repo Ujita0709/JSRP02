e5216a4a6ad4266ecd1010ffc13f14e05ce4d246
Hiroki_ujita <hiroki_ujita@hotmail.com>
2025-11-27 14:04:34 +0900

    問題5までのUserCard.jsx

---
+++ src/UserCard1.jsx
@@ -0,0 +1,30 @@
+function UserCard({ name, age, hobby, introduce, bgColor}) {
+//introduce:問題2、bgcolor:問題1
+  // 年齢によるデフォルト背景色（30歳以上なら薄い赤）：問題3
+  const defaultBgColor = age >= 30 ? "#ffe5e5" : "#f9f9f9";  
+  // インラインスタイル
+  const cardStyle = {
+    border: "1px solid #ccc",
+    padding: "16px",
+    marginBottom: "12px",
+    borderRadius: "8px",
+    backgroundColor: bgColor || defaultBgColor,//問題1
+    cursor: "pointer",
+  };
+
+  //クリックしたら名前を表示：問題4
+  const handleClick = () =>{
+    alert(`こんにちは、${name}です！`)
+  }
+
+  return (
+    <div style={cardStyle} onClick={handleClick}>{/*問題4*/}
+      <h2>{name}</h2>
+      <p>年齢：{age} 歳</p>
+      <p>趣味：{hobby}</p>
+      {/* introduce が渡されていれば表示:問題2 */}
+      {introduce && <p>自己紹介：{introduce}</p>}
+    </div>
+  );
+}
+export default UserCard;
