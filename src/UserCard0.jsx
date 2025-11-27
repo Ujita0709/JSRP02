dc392479dacedaf3ddcaa60993d36ca30b39abfe
Hiroki_ujita <hiroki_ujita@hotmail.com>
2025-11-27 11:26:58 +0900

    問題追加前のUserCard.jsx

---
+++ src/UserCard0.jsx
@@ -0,0 +1,19 @@
+function UserCard({ name, age, hobby }) {
+  // インラインスタイル
+  const cardStyle = {
+    border: "1px solid #ccc",
+    padding: "16px",
+    marginBottom: "12px",
+    borderRadius: "8px",
+    backgroundColor: "#f9f9f9"
+  };
+
+  return (
+    <div style={cardStyle}>
+      <h2>{name}</h2>
+      <p>年齢：{age} 歳</p>
+      <p>趣味：{hobby}</p>
+    </div>
+  );
+}
+export default UserCard;
